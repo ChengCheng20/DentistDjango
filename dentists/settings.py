054d9da60ccc16fb295389b7731b2755f06655e9
ChengCheng20 <64708843+ChengCheng20@users.noreply.github.com>
2020-06-07 23:49:30 +0200

    Update settings.py
    New SK generated hidden in settings

--- dentists/settings.py
+++ dentists/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'dx@w88sz-dni_$ypc5o6sxnfof+jdg%od(hfar-6md=5^4=cvo'
+SECRET_KEY= os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
